21af397553154363e2a431ae5e85a59776230f48
German <gchistopashin@mail.ru>
2025-05-22 11:15:24 +0300

    add free UI element for Build

--- Assets/BuildSystem/Scripts/UIBuildActivator.cs
+++ Assets/BuildSystem/Scripts/UIBuildActivator.cs
@@ -12,7 +12,20 @@ public class UIBuildActivator : MonoBehaviour
 
     void SetPosition(IInteractableObj buildObj)
     {
-        this.GetComponent<RectTransform>().position = new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0);
-        print($"{new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0)} - {this.GetComponent<RectTransform>().position}");
+        Vector3 mousePosOnScreen = new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0);
+        RectTransform rectTransform = this.GetComponent<RectTransform>();
+        float x, y;
+
+        if (mousePosOnScreen.x < Screen.width / 2)
+            x = (mousePosOnScreen - (rectTransform.sizeDelta.x / 2 * Vector3.left)).x;
+        else
+            x = (mousePosOnScreen + (rectTransform.sizeDelta.x / 2 * Vector3.left)).x;
+
+        if (mousePosOnScreen.y < Screen.height / 2)
+            y = (mousePosOnScreen + (rectTransform.sizeDelta.y / 2 * Vector3.up)).y;
+        else
+            y = (mousePosOnScreen - (rectTransform.sizeDelta.y / 2 * Vector3.up)).y;
+
+        rectTransform.anchoredPosition = Vector3.up * y + Vector3.right * x;
     }
 }

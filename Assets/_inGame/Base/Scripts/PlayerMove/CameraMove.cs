c4466d606e50a826eae12467cc821297590ce151
deathlydh <mr.artem1056@gmail.com>
2025-07-29 13:17:05 +0500

    fix problems master branch

--- Assets/_inGame/Base/Scripts/PlayerMove/CameraMove.cs
+++ Assets/_inGame/Base/Scripts/PlayerMove/CameraMove.cs
@@ -12,11 +12,11 @@ public class CameraMove : MonoBehaviour
     private Vector3 _inputVector;
     private Vector3 _moveVector = Vector3.zero;
     private bool IsHovered = false;
-    
-    [FormerlySerializedAs("MaxSpeed")] [SerializeField] private float _maxSpeed;
-    [FormerlySerializedAs("MinSpeed")] [SerializeField] private float _minSpeed;
-    [FormerlySerializedAs("SpeedUp")] [SerializeField] private float _speedUp;
-    [FormerlySerializedAs("CameraPlayer")] [SerializeField] private GameObject _cameraPlayer;
+
+    [FormerlySerializedAs("MaxSpeed")][SerializeField] private float _maxSpeed;
+    [FormerlySerializedAs("MinSpeed")][SerializeField] private float _minSpeed;
+    [FormerlySerializedAs("SpeedUp")][SerializeField] private float _speedUp;
+    [FormerlySerializedAs("CameraPlayer")][SerializeField] private GameObject _cameraPlayer;
 
     void Start()
     {
@@ -26,8 +26,8 @@ public class CameraMove : MonoBehaviour
 
     void CheckInput()
     {
-        _inputVector.x = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ?  1 : 0;
-        _inputVector.z = Input.GetKey(KeyCode.W) ?  1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+        _inputVector.x = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0;
+        _inputVector.z = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
 
 
         if (Input.GetKey(KeyCode.LeftControl))
@@ -43,7 +43,7 @@ public class CameraMove : MonoBehaviour
         _inputVector.z = vertical;
         IsHovered = vertical != 0;
     }
-    
+
     public void HorizontalScreenTouch(float horizontal)
     {
         _inputVector.x = horizontal;
@@ -60,27 +60,27 @@ public class CameraMove : MonoBehaviour
     void MoveScore()
     {
         Vector3 moveVector = (
-            _player.transform.right * _inputVector.x + 
-                             _player.transform.forward * _inputVector.z + 
+            _player.transform.right * _inputVector.x +
+                             _player.transform.forward * _inputVector.z +
                              _cameraPlayer.transform.forward * _inputVector.y
             ).normalized * _speedUp;
-        
-            if (moveVector != Vector3.zero)
-            {
-                _moveVector = Vector3.Lerp(_moveVector, moveVector, _speedUp * Time.deltaTime);
-            }
-            else
-            {
-                _moveVector = Vector3.Lerp(_moveVector, Vector3.zero, _speedUp*10 * Time.deltaTime);
-            }
-            
-            if (_moveVector.magnitude > _maxSpeed)
-                _moveVector = _moveVector.normalized * _maxSpeed;
+
+        if (moveVector != Vector3.zero)
+        {
+            _moveVector = Vector3.Lerp(_moveVector, moveVector, _speedUp * Time.deltaTime);
+        }
+        else
+        {
+            _moveVector = Vector3.Lerp(_moveVector, Vector3.zero, _speedUp * 10 * Time.deltaTime);
+        }
+
+        if (_moveVector.magnitude > _maxSpeed)
+            _moveVector = _moveVector.normalized * _maxSpeed;
     }
-    
+
     void Update()
     {
-        if(!IsHovered)
+        if (!IsHovered)
             CheckInput();
         MoveScore();
         this.transform.position += _moveVector;
--- Assets/_inGame/Base/Scripts/PlayerMove/CameraRotate.cs
+++ Assets/_inGame/Base/Scripts/PlayerMove/CameraRotate.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CameraRotate : MonoBehaviour
 {
     private RaycastHit _hit;
-    
+
     [SerializeField] private GameObject _playerCamera;
-    [SerializeField] private float      RotationSpeed;
-    private float      _rotation;
+    [SerializeField] private float RotationSpeed;
+    private float _rotation;
 
     public void RotateFromUI(float horizontal)
     {
@@ -23,12 +23,12 @@ public class CameraRotate : MonoBehaviour
     void Update()
     {
         Vector3 preVector = transform.position - _hit.point;
-        
-        _rotation = Input.GetKey(KeyCode.Q) ? RotationSpeed:
-            Input.GetKey(KeyCode.E) ? -RotationSpeed  : 0;
-        this.transform.RotateAround(_hit.point, Vector3.up,
-            (GlobalInteractEvent.IsLockOnUI? (Input.GetMouseButton(0) ? _rotation: 0.0f): _rotation) * Time.deltaTime);
 
-    
+        _rotation = Input.GetKey(KeyCode.Q) ? RotationSpeed :
+            Input.GetKey(KeyCode.E) ? -RotationSpeed : 0;
+        this.transform.RotateAround(_hit.point, Vector3.up,
+            (GlobalInteractEvent.IsLockOnUI ? (Input.GetMouseButton(0) ? _rotation : 0.0f) : _rotation) * Time.deltaTime);
+
+
     }
 }
